b3b2efbae0514fd7963c79ee73772972dbb86d68
Thanut Panichyotai <luvikung@gmail.com>
2025-05-12 17:33:19 +0700

    Trying to use local storage but didn't work.

--- src/app/components/ClientThemeProvider.tsx
+++ src/app/components/ClientThemeProvider.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import React, { useState, useMemo, createContext, useContext } from 'react';
+import React, { useCallback, useMemo, createContext, useContext } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import useLocalStorage from 'use-local-storage';
 
 export enum Mode {
   Light = 'light',
@@ -14,6 +15,7 @@ interface ThemeContextType {
   mode: Mode;
   toggleMode: () => void;
 }
+
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const useThemeContext = (): ThemeContextType => {
@@ -30,10 +32,13 @@ interface ClientThemeProviderProps {
 
 const ClientThemeProvider: React.FC<ClientThemeProviderProps> = ({ children }) => {
   const systemPrefersDark = useMediaQuery('(prefers-color-scheme: dark)');
-  const [mode, setMode] = useState<Mode>(systemPrefersDark ? Mode.Dark : Mode.Light);
-  const toggleMode = () => {
-    setMode((prevMode) => (prevMode === Mode.Light ? Mode.Dark : Mode.Light));
-  };
+  const [mode, setMode] = useLocalStorage<Mode>(
+    'themeMode',
+    systemPrefersDark ? Mode.Dark : Mode.Light
+  );
+  const toggleMode = useCallback(() => {
+    setMode(mode === Mode.Light ? Mode.Dark : Mode.Light);
+  }, [mode, setMode]);
   const theme = useMemo(() => createTheme({ palette: { mode } }), [mode]);
   return (
     <ThemeContext.Provider value={{ mode, toggleMode }}>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import ClientThemeProvider from './components/ClientThemeProvider';
+import ClientThemeProvider from '@/app/components/ClientThemeProvider';
 import './global.css';
 
 export const metadata: Metadata = {
