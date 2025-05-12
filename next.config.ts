2609b332212870d3cccad0a7b7cbd35067bc85ac
Thanut Panichyotai <luvikung@gmail.com>
2025-05-12 18:46:43 +0700

    Improve builds.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'out',
+  // basePath: '/base',
+  // assetPrefix: '/base/',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
